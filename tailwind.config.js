66613e1a1ef99a5087550b476b949f9b34f39f38
michal slowikowski <michalslowikowski00@gmail.com>
2025-03-14 17:33:29 +0100

    add components

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    container: {
+      center: true,
+      padding: "15px",
+    },
+  },
+  extend: {},
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
