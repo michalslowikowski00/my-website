c61e23993636eda7501699c1f8d0fb1e1ea2414e
michalslowikowski <michalslowikowski00@gmail.com>
2025-03-25 10:57:42 +0100

    add stats

--- src/components/customs/Stats.tsx
+++ src/components/customs/Stats.tsx
@@ -1,16 +1,43 @@
+'use client';
+
+import CountUp from 'react-countup';
+
+const statistics = [
+  { year: 10, text: 'Years of expirience' },
+  { year: 7, text: 'Projects involved' },
+  { year: 9, text: 'Technology stack' },
+  { year: 100, text: 'Github commits' },
+];
+
 export const Stats = () => {
   return (
-    <section>
+    <section className="pt-4 pb-12 xl:pt-0 xl:-pb-0">
       <div className="container mx-auto">
-        <div className="flex flex-wrap mx-auto gap-6 ">
-          <p className="text-8xl font-extrabold">12</p>
-          <span className="wrap justify-baseline font-bold w-full">Years of expirience</span>
+        <div className="flex flex-wrap mx-auto gap-6 max-w-[80vw] xl-max-w-none">
+          {statistics.map((stat, index) => {
+            return (
+              <div
+                key={index}
+                className="flex-1 flex gap-4 items-center justify-content xl:justify-start"
+              >
+                <CountUp
+                  start={0}
+                  end={stat.year}
+                  duration={7}
+                  delay={2}
+                  className="text-8xl font-extrabold text-white/60"
+                />
+                <p
+                  className={`${stat.text.length < 15 ? 'max-w-[100px]' : 'max-w-[150px]'} 
+              leading-snug text-white/70 text-xl`}
+                >
+                  {stat.text}
+                </p>
+              </div>
+            );
+          })}
         </div>
       </div>
     </section>
   );
 };
-
-// text-align: center;
-// vertical-align: middle;
-// line-height: 90px;
