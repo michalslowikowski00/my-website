5cf5c27a4e7ae0090355013c6860e8503eb84a35
michalslowikowski <michalslowikowski00@gmail.com>
2025-04-01 09:29:53 +0200

    add icons to the skills

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -2,7 +2,13 @@
 
 import { ScrollArea } from '@/components/ui/scroll-area';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import {
+  Tooltip,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
 import { motion } from 'framer-motion';
+import { FaHtml5 } from 'react-icons/fa';
 
 const Resume = () => {
   return (
@@ -21,7 +27,6 @@ const Resume = () => {
         >
           <TabsList className="flex flex-col w-full max-w-[380px] mx-auto gap-6 xl:mx-0">
             <TabsTrigger value="experience">experience</TabsTrigger>
-            <TabsTrigger value="education">education</TabsTrigger>
             <TabsTrigger value="skills">skills</TabsTrigger>
             <TabsTrigger value="about me">about me</TabsTrigger>
           </TabsList>
@@ -65,20 +70,34 @@ const Resume = () => {
               </div>
             </TabsContent>
 
-            {/* EDU */}
-            <TabsContent
-              value="education"
-              className="w-full"
-            >
-              education
-            </TabsContent>
-
             {/* SKILL */}
             <TabsContent
               value="skills"
               className="w-full"
             >
-              skills
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{skillSet.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                  {skillSet.description}
+                </p>
+                <ScrollArea className="h-[400px]">
+                  <ul className="grid lg:grid-cols-4 gap-5 w-max-[50px]">
+                    {skillSet.items.map((item, index) => {
+                      return (
+                        <li key={index}>
+                          <TooltipProvider>
+                            <Tooltip>
+                              <TooltipTrigger className="text-5xl">
+                                {item.icon}
+                              </TooltipTrigger>
+                            </Tooltip>
+                          </TooltipProvider>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </ScrollArea>
+              </div>
             </TabsContent>
 
             {/* ABOUT */}
@@ -108,7 +127,6 @@ const aboutMe = {
 };
 
 const expirience = {
-  icon: '',
   title: 'My expirience',
   description: 'Yo, yo, check this out, man!',
   items: [
@@ -136,6 +154,70 @@ const expirience = {
       company: 'SeaChange',
       position: 'Full Stack Engineer',
       duration: '2016 - 2017',
+    },
+  ],
+};
+
+const skillSet = {
+  title: 'Skill set',
+  description: 'This thigs I know, well or enough',
+  items: [
+    {
+      skill: 'typesctipt',
+      icon: <FaHtml5 />,
+    },
+
+    {
+      skill: 'python',
+      icon: '',
+    },
+    {
+      skill: 'Kotlin',
+      icon: '',
+    },
+    {
+      skill: 'sql',
+      icon: '',
+    },
+    {
+      skill: 'React',
+      icon: '',
+    },
+    {
+      skill: 'Tailwind CSS',
+      icon: '',
+    },
+    {
+      skill: 'Shadcn',
+      icon: '',
+    },
+    {
+      skill: 'Next.JS',
+      icon: '',
+    },
+    {
+      skill: 'Express',
+      icon: '',
+    },
+    {
+      skill: 'Playwright',
+      icon: '',
+    },
+    {
+      skill: 'Jest',
+      icon: '',
+    },
+    {
+      skill: 'Pytest',
+      icon: '',
+    },
+    {
+      skill: 'Selenium',
+      icon: '',
+    },
+    {
+      skill: 'Cypress',
+      icon: '',
     },
   ],
 };
