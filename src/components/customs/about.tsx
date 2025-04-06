be97f61691c41f57b90b49246cd6a0a8a7399fdb
michalslowikowski <michalslowikowski00@gmail.com>
2025-04-06 19:50:36 +0200

    add iframe for yt videos

--- src/components/customs/about.tsx
+++ src/components/customs/about.tsx
@@ -5,16 +5,10 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from '@/components/ui/carousel';
-import { Card, CardContent } from '../ui/card';
+import { CardContent } from '../ui/card';
 import { TabsContent } from '../ui/tabs';
-import Image from 'next/image';
 
-const pictures = [
-  '/assets/image1.png',
-  '/assets/image2.png',
-  '/assets/image3.png',
-  '/assets/image.png',
-];
+const params = ['tHyfAwEmzxE', 'fhOLSQtpygg', 'M28RCeAZnnE', 'uFPaGN3Hwmk'];
 
 export const AboutMe = () => {
   return (
@@ -23,29 +17,26 @@ export const AboutMe = () => {
       className="w-full"
     >
       <div className="flex flex-col gap-[30px] text-center xl:text-left">
-        <h3 className="text-4xl font-bold">Hey there...</h3>
+        <h3 className="text-4xl font-bold">What&apos;s up?</h3>
         <p className="max-w-[600px] text-2xl text-white/60 mx-auto xl:mx-0">
-          Here you can read a bit about me
+          Here you can see what I do in the free time
         </p>
       </div>
       <div className="mx-10 py-11 max-w-lg w-full">
-        <Carousel className="w-full ">
+        <Carousel className="w-3xl">
           <CarouselContent>
-            {pictures.map((picture, index) => {
+            {params.map((param, index) => {
               return (
                 <CarouselItem key={index}>
-                  <Card>
-                    <CardContent className="flex aspect-square items-center justify-center p-6">
-                      <Image
-                        src={picture}
-                        priority
-                        alt={'guitar'}
-                        quality={100}
-                        fill
-                        className="object-contain"
-                      />
-                    </CardContent>
-                  </Card>
+                  <CardContent className="flex aspect-video items-center justify-center p-6 w-full">
+                    <iframe
+                      width="100%"
+                      height="100%"
+                      src={`https://www.youtube.com/embed/${param}`}
+                      allow="autoplay; encrypted-media"
+                      title="video"
+                    />
+                  </CardContent>
                 </CarouselItem>
               );
             })}
