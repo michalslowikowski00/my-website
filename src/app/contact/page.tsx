86dfc128bce9daf4c1dc6cd754c3a0191be638e4
michalslowikowski <michalslowikowski00@gmail.com>
2025-04-07 12:19:25 +0200

    add form

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,42 @@
+import { Input } from '@/components/ui/input';
+
 const Contact = () => {
-  return <div className="text-white">Contact page</div>;
+  return (
+    <div className="container mx-auto">
+      <div className="flex flex-col xl:flex-row gap-6 ">
+        <div>
+          <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
+            <h3 className="text-4xl text-emerald-600 font-bold">
+              Want to work with me?
+            </h3>
+            <p className="text-white/60">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua
+            </p>
+            <div className="grid grid-cols-1 gap-6">
+              <Input
+                type="firstname"
+                placeholder="Firstname"
+              />
+              <Input
+                type="lastname"
+                placeholder="Lastname"
+              />
+              <Input
+                type="email"
+                placeholder="Email"
+              />
+              <Input
+                type="phone"
+                placeholder="Phone number"
+              />
+            </div>
+          </form>
+        </div>
+        <div>contact data</div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
--- src/components/customs/Nav.tsx
+++ src/components/customs/Nav.tsx
@@ -5,7 +5,6 @@ import { usePathname } from 'next/navigation';
 const links = [
   { name: 'home', path: '/' },
   { name: 'resume', path: '/resume' },
-  { name: 'work', path: '/work' },
   { name: 'contact', path: '/contact' },
 ];
 
